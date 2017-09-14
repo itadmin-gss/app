336988a5d66d97e71c79ad6db587422672ebc4c4
Laravel Shift <shift@laravelshift.com>
2017-09-14 18:51:52 +0000

    Shift core files

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,14 +1,15 @@
 <?php namespace App\Http\Controllers;
 
+use Illuminate\Foundation\Auth\Access\AuthorizesResources;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 
-abstract class Controller extends BaseController
+class Controller extends BaseController
 {
 
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    use AuthorizesRequests, AuthorizesResources, DispatchesJobs, ValidatesRequests;
 
     /**
      * Setup the layout used by the controller.
