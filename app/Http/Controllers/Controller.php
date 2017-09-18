568ece08bad76c30097e2181d607c196da8f6185
Jason McCreary <jason@pureconcepts.net>
2017-09-18 16:08:50 -0400

    Rework shared view data

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,84 +1,22 @@
 <?php namespace App\Http\Controllers;
 
-use App\CustomerType;
-use App\RoleDetail;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Notification;
-use Illuminate\Support\Facades\Session;
-use Illuminate\Support\Facades\View;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
-    /**
-     * Setup the layout used by the controller.
-     *sdfsd
-     * @return void
-     */
-    protected function setupLayout()
-    {
-        if (!is_null($this->layout)) {
-            $this->layout = view($this->layout);
-        }
-    }
-
-
-    public function __construct()
-    {
-        if (Auth::check()) {
-            $role_id = Auth::user()->user_role_id;
-
-            $role_detail = RoleDetail::where('role_id', '=', $role_id)
-                ->get();
-
-            $access_roles = [];
-
-
-            foreach ($role_detail as $role) {
-                $rol_func = $role->roleFunctions->role_function;
-
-
-                $access_roles[$rol_func]['add'] = $role['add'];
-                $access_roles[$rol_func]['edit'] = $role['edit'];
-                $access_roles[$rol_func]['delete'] = $role['delete'];
-                $access_roles[$rol_func]['view'] = $role['view'];
-            }
-
-            View::share('access_roles', $access_roles);
-            $getNotifications = Notification::getNotifications(Auth::user()->id);
-            $unreadnotifications = Notification::where('recepient_id', '=', Auth::user()->id)
-                ->where('is_read', '=', 1)
-                ->skip(0)
-                ->take(5)
-                ->count();
-            $CustomerType = CustomerType::get();
-            $clientTypeSession = Session::get('clientType');
-            View::share('get_notifications', $getNotifications);
-            View::share('unreadnotifications', $unreadnotifications);
-            View::share('CustomerType', $CustomerType);
-            View::share('clientTypeSession', $clientTypeSession);
-        }
-    }
-
     public function notify()
     {
-        $getNotifications = Notification::getNotifications(Auth::user()->id);
-        $unreadnotifications = Notification::where('recepient_id', '=', Auth::user()->id)
+        return Notification::where('recepient_id', '=', Auth::user()->id)
             ->where('is_read', '=', 1)
             ->skip(0)
             ->take(5)
             ->count();
-        $CustomerType = CustomerType::get();
-        $clientTypeSession = Session::get('clientType');
-
-        //View::share('get_notifications', $getNotifications);
-        return $unreadnotifications;
-        //View::share('CustomerType', $CustomerType);
-        //View::share('clientTypeSession', $clientTypeSession);
     }
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,5 +1,11 @@
 <?php namespace App\Providers;
 
+use App\CustomerType;
+use App\Notification;
+use App\RoleDetail;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -12,7 +18,39 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        View::composer('*', function($view) {
+            if (Auth::check()) {
+                $role_id = Auth::user()->user_role_id;
+
+                $role_detail = RoleDetail::where('role_id', '=', $role_id)->get();
+
+                $access_roles = [];
+
+                foreach ($role_detail as $role) {
+                    $rol_func = $role->roleFunctions->role_function;
+
+
+                    $access_roles[$rol_func]['add'] = $role['add'];
+                    $access_roles[$rol_func]['edit'] = $role['edit'];
+                    $access_roles[$rol_func]['delete'] = $role['delete'];
+                    $access_roles[$rol_func]['view'] = $role['view'];
+                }
+
+                $view->with('access_roles', $access_roles);
+                $getNotifications = Notification::getNotifications(Auth::user()->id);
+                $unreadnotifications = Notification::where('recepient_id', '=', Auth::user()->id)
+                    ->where('is_read', '=', 1)
+                    ->skip(0)
+                    ->take(5)
+                    ->count();
+                $CustomerType = CustomerType::get();
+                $clientTypeSession = Session::get('clientType');
+                $view->with('get_notifications', $getNotifications);
+                $view->with('unreadnotifications', $unreadnotifications);
+                $view->with('CustomerType', $CustomerType);
+                $view->with('clientTypeSession', $clientTypeSession);
+            }
+        });
     }
 
     /**
