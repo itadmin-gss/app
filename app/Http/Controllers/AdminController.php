925121efbe6aae58b9c5ea07257802af43d1aac1
Jamie Dunn <jdunn82k@gmail.com>
2017-10-09 10:10:59 -0400

    Vendor Email testing

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -266,9 +266,9 @@ class AdminController extends Controller
                 {
                     //Email New Vendor with Link to Login
                     $email_data = [
-                        'first_name' => $data->first_name,
-                        'username' => $data->username,
-                        'email' => $data->email,
+                        'first_name' => $data['first_name'],
+                        'username' => $data['username'],
+                        'email' => $data['email'],
                         'token' => 'email-link/vendor/'.$token,
                     ];
 
