95259efc11bf0c459a3cf52bd66212a440ce667c
Jamie Dunn <jdunn82k@gmail.com>
2017-10-26 21:03:05 -0400

    Enabled the Vendor/Properties links in top nav bar

--- resources/views/common/sidebar.blade.php
+++ resources/views/common/sidebar.blade.php
@@ -146,13 +146,13 @@
       <ul class="navbar-nav ml-auto">
         
         <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Vendors">
-            <a class="nav-link" href="javascript:void(0)">
+            <a class="nav-link" href="{!!URL::to('list-vendors') !!}">
               <i class="fa fa-fw fa-users"></i>
               <span class="nav-link-text">Vendors</span>
             </a>
           </li>
           <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Properties">
-            <a class="nav-link" href="javascript:void(0)">
+            <a class="nav-link" href="{!!URL::to('list-assets') !!}">
               <i class="fa fa-fw fa-home"></i>
               <span class="nav-link-text">Properties</span>
             </a>
