139a75238e0b901a46eafe6981ad8bc26d54b2fa
Jamie Dunn <jdunn82k@gmail.com>
2017-11-07 00:00:41 -0500

    Added title to Vendor Dashboard

--- resources/views/pages/vendors/dashboard.blade.php
+++ resources/views/pages/vendors/dashboard.blade.php
@@ -3,28 +3,12 @@
 @extends('layouts.vendordashboard')
 @section('content')
 
-
+<title>GSS - Vendor Dashboard</title>
 <div id="content" class="span11">
-
-
-
-
     <div class="row-fluid">
-
-
-
-
-
         <div class="box span12 main-title">
-
-
             <h1>Vendor Dashboard</h1>
-
-
         </div><!--/span-->
-
-
-
 </div>
 
 
