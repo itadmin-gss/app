f565680248b4f0b2a498def3179e70f5c9e6122a
Jamie Dunn <jdunn82k@gmail.com>
2018-01-12 14:42:05 -0500

    Basic styling for Edit Profile (self) page

--- resources/views/common/edit_profile.blade.php
+++ resources/views/common/edit_profile.blade.php
@@ -8,13 +8,9 @@
 
     <div class="row-fluid">
 
-        <div class="box span12">
-
-            <div class="box-header" data-original-title>
-
                 <h2><span class="break"></span>Profile Edit</h2>
 
-            </div>
+
 
             <div class="box-content custome-form clearfix">
 
@@ -50,9 +46,9 @@
 
                 @endif
 
-                <div class="row-fluid">
+                <div class="row">
 
-                    <div class="span12">
+                    <div class="col-md-6">
 
                         {!! Form::open(array('url' => 'save-profile', 'files'=>true, 'id'=>'profileEditForm' , 'class'=>'form-horizontal')) !!}
 
@@ -61,18 +57,15 @@
                             <div class="row-fluid">
 
                                 <div class="span6">
-
-
-
                                     <div class="control-group">
 
                                         {!!Form::label('username', 'Username', array('class' => 'control-label'))!!}
 
-                                        <div class="controls">
+                                        <div class="form-inline">
 
                                             @if($user_data['username'] == '' || !isset($user_data['username']))
 
-                                            {!! Form::text('username',  isset($user_data['username']) ? $user_data['username'] : '' , array('class'=>'span10 typeahead','id'=>'username'))!!} 
+                                            {!! Form::text('username',  isset($user_data['username']) ? $user_data['username'] : '' , array('class'=>'form-control','id'=>'username'))!!}
 
                                              {!!Form::hidden('check_user_name', 'yes')!!}  
 
@@ -80,7 +73,7 @@
 
                                             @else
 
-                                            {!! Form::text('username',  isset($user_data['username']) ? $user_data['username'] : '' , array('class'=>'span10 typeahead','id'=>'username',  'readonly'=>'true'))!!}
+                                            {!! Form::text('username',  isset($user_data['username']) ? $user_data['username'] : '' , array('class'=>'form-control','id'=>'username',  'readonly'=>'true'))!!}
 
                                             {!!Form::hidden('check_user_name', 'no')!!}
 
@@ -100,9 +93,9 @@
 
                                         {!!Form::label('email', 'Email', array('class' => 'control-label'))!!}
 
-                                        <div class="controls">
+                                        <div class="form-inline">
 
-                                            {!! Form::email('email',  isset($user_data['email']) ? $user_data['email'] : '' , array('class'=>'span10 typeahead','id'=>'email', 'readonly'=>'true'))!!}
+                                            {!! Form::email('email',  isset($user_data['email']) ? $user_data['email'] : '' , array('class'=>'form-control','id'=>'email', 'readonly'=>'true'))!!}
 
                                         </div>
 
@@ -112,9 +105,9 @@
 
                                         {!!Form::label('first_name', 'First Name', array('class' => 'control-label'))!!}
 
-                                        <div class="controls">
+                                        <div class="form-inline">
 
-                                            {!! Form::text('first_name',  isset($user_data['first_name']) ? $user_data['first_name'] : '' , array('class'=>'span10 typeahead','id'=>'firstName','readonly'=>'true'))!!}
+                                            {!! Form::text('first_name',  isset($user_data['first_name']) ? $user_data['first_name'] : '' , array('class'=>'form-control','id'=>'firstName','readonly'=>'true'))!!}
 
                                             {!! Form::button('Edit', array('class'=>'btn btn-small btn-success','id'=>'editFirstName'))!!}
 
@@ -126,9 +119,9 @@
 
                                         {!!Form::label('last_name', 'Last Name', array('class' => 'control-label'))!!}
 
-                                        <div class="controls">
+                                        <div class="form-inline">
 
-                                            {!! Form::text('last_name',  isset($user_data['last_name']) ? $user_data['last_name'] : '' , array('class'=>'span10 typeahead','id'=>'lastName','readonly'=>'true'))!!}
+                                            {!! Form::text('last_name',  isset($user_data['last_name']) ? $user_data['last_name'] : '' , array('class'=>'form-control','id'=>'lastName','readonly'=>'true'))!!}
 
                                             {!! Form::button('Edit', array('class'=>'btn btn-small btn-success','id'=>'editLastName'))!!}
 
@@ -138,7 +131,7 @@
                                                        @if($user_data['type_id']==2)
                                <!--      <div class="control-group">
                                         {{--  {!!Form::label('customer_type_id', 'Customer Type', array('class' => 'control-label'))!!}  --}}
-                                        <div class="controls">
+                                        <div class="form-inline">
                                            <select name="customer_type_id" id="customer_type_id">
                                                <?php
 
@@ -162,9 +155,9 @@
 
                                         {!!Form::label('company', 'Company', array('class' => 'control-label'))!!}
 
-                                        <div class="controls">
+                                        <div class="form-inline">
 
-                                            {!! Form::text('company',  isset($user_data['company']) ? $user_data['company'] : '' ,  array('class'=>'span10 typeahead','id'=>'company','readonly'=>'true'))!!}
+                                            {!! Form::text('company',  isset($user_data['company']) ? $user_data['company'] : '' ,  array('class'=>'form-control','id'=>'company','readonly'=>'true'))!!}
 
                                             {!! Form::button('Edit', array('class'=>'btn btn-small btn-success','id'=>'editCompany'))!!}
 
@@ -178,9 +171,9 @@
 
                                         {!!Form::label('phone', 'Phone', array('class' => 'control-label'))!!}
 
-                                        <div class="controls">
+                                        <div class="form-inline">
 
-                                            {!! Form::text('phone', isset($user_data['phone']) ? $user_data['phone'] : '' , array('class'=>'span10 typeahead','id'=>'phone','readonly'=>'true'))!!}
+                                            {!! Form::text('phone', isset($user_data['phone']) ? $user_data['phone'] : '' , array('class'=>'form-control','id'=>'phone','readonly'=>'true'))!!}
 
                                             {!! Form::button('Edit', array('class'=>'btn btn-small btn-success','id'=>'editPhone'))!!}
 
@@ -196,9 +189,9 @@
 
                                         {!!Form::label('address_1', 'Address 1', array('class' => 'control-label'))!!}
 
-                                        <div class="controls">
+                                        <div class="form-inline">
 
-                                            {!! Form::text('address_1', isset($user_data['address_1']) ? $user_data['address_1'] : ''  , array('class'=>'span10 typeahead','id'=>'address1','readonly'=>'true'))!!}
+                                            {!! Form::text('address_1', isset($user_data['address_1']) ? $user_data['address_1'] : ''  , array('class'=>'form-control','id'=>'address1','readonly'=>'true'))!!}
 
                                             {!! Form::button('Edit', array('class'=>'btn btn-small btn-success','id'=>'editAddress1'))!!}
 
@@ -212,9 +205,9 @@
 
                                         {!!Form::label('address_2', 'Address 2', array('class' => 'control-label'))!!}
 
-                                        <div class="controls">
+                                        <div class="form-inline">
 
-                                            {!! Form::text('address_2', isset($user_data['address_2']) ? $user_data['address_2'] : '' , array('class'=>'span10 typeahead','id'=>'address2','readonly'=>'true'))!!}
+                                            {!! Form::text('address_2', isset($user_data['address_2']) ? $user_data['address_2'] : '' , array('class'=>'form-control','id'=>'address2','readonly'=>'true'))!!}
 
                                             {!! Form::button('Edit', array('class'=>'btn btn-small btn-success','id'=>'editAddress2'))!!}
 
@@ -234,39 +227,39 @@
 
 
 
-                                <div class="span6 text-center">
+                                {{--<div class="span6">--}}
+                                    {{--<h3>Profile Picture</h3>--}}
+                                    {{--<div class="control-group">--}}
 
-                                    <div class="control-group">
+                                        {{--<div class="fileinput fileinput-new" data-provides="fileinput">--}}
 
-                                        <div class="fileinput fileinput-new" data-provides="fileinput">
+                                            {{--<div class="fileinput-preview thumbnail" data-trigger="fileinput" style="width: 200px; height: 150px;">--}}
 
-                                            <div class="fileinput-preview thumbnail" data-trigger="fileinput" style="width: 200px; height: 150px;">
+                                                {{--@if($user_data['profile_picture'])--}}
 
-                                                @if($user_data['profile_picture'])
+                                                {{--{!! Html::image(Config::get('app.upload_path').$user_data['profile_picture']) !!}--}}
 
-                                                {!! Html::image(Config::get('app.upload_path').$user_data['profile_picture']) !!}
+                                                {{--@endif--}}
 
-                                                @endif
+                                            {{--</div>--}}
 
-                                            </div>
+                                            {{--<div>--}}
 
-                                            <div>
+                                                {{--<span class="btn btn-default btn-file">--}}
 
-                                                <span class="btn btn-default btn-file">
+                                                    {{--{!!Form::file('profile_picture', array('value'=>$user_data['profile_picture'],'class'=>'input-file uniform_on', 'id'=>'fileInput'))!!}--}}
 
-                                                    {!!Form::file('profile_picture', array('value'=>$user_data['profile_picture'],'class'=>'input-file uniform_on', 'id'=>'fileInput'))!!}
+                                                {{--</span>--}}
 
-                                                </span>
+                                                {{--<a href="#" class="btn btn-default fileinput-exists" data-dismiss="fileinput">Remove</a>--}}
 
-                                                <a href="#" class="btn btn-default fileinput-exists" data-dismiss="fileinput">Remove</a>
+                                            {{--</div>--}}
 
-                                            </div>
+                                        {{--</div>--}}
 
-                                        </div>
+                                    {{--</div>--}}
 
-                                    </div>
-
-                                </div>
+                                {{--</div>--}}
 
                                 <!--/span-4-->
 
@@ -300,7 +293,7 @@
 
 
 
-                                    <div class="controls">
+                                    <div class="form-inline">
 
                                         {!! Form::select('state_id', $states_data , isset($user_data['state_id']) ? $user_data['state_id'] : '0', array('class'=>'span8 typeahead','id'=>'state_id', 'data-rel'=>'chosen'))!!}
 
@@ -330,9 +323,9 @@
 
                                     {!!Form::label('city_id', 'City', array('class' => 'control-label first-label'))!!}
 
-                                    <div class="controls">
+                                    <div class="form-inline">
 
-                                        {!! Form::select('city_id', $city_data , isset($user_data['city_id']) ? $user_data['city_id'] : '0' , array('style'=>'width:200px;' ,'class'=>'span7 typeahead','id'=>'city_id', 'data-rel'=>'chosen'))!!}
+                                        {!! Form::select('city_id', $city_data , isset($user_data['city_id']) ? $user_data['city_id'] : '0' , array('style'=>'width:200px;' ,'class'=>'form-control','id'=>'city_id', 'data-rel'=>'chosen'))!!}
 
 
 
@@ -348,9 +341,9 @@
 
 
 
-                                    <div class="controls">
+                                    <div class="form-inline">
 
-                                        {!! Form::text('zipcode', isset($user_data['zipcode']) ? $user_data['zipcode'] : '' , array('class'=>'span7 typeahead','id'=>'zipcode','readonly'=>'true'))!!}
+                                        {!! Form::text('zipcode', isset($user_data['zipcode']) ? $user_data['zipcode'] : '' , array('class'=>'form-control','id'=>'zipcode','readonly'=>'true'))!!}
 
                                         {!! Form::button('Edit', array('class'=>'btn btn-small btn-success','id'=>'editZipcode'))!!}
 
@@ -406,9 +399,9 @@
 
                                     {!!Form::label('current_password', 'Current Password: *', array('class'=>'control-label', 'for'=>'typeahead'))!!}
 
-                                    <div class="controls">
+                                    <div class="form-inline">
 
-                                        {!! Form::password('current_password', array('class'=>'span10 typeahead','id'=>'password'))!!}
+                                        {!! Form::password('current_password', array('class'=>'form-control','id'=>'password'))!!}
 
                                     </div>
 
@@ -418,9 +411,9 @@
 
                                     {!!Form::label('password', 'New Password: *', array('class'=>'control-label', 'for'=>'typeahead'))!!}
 
-                                    <div class="controls">
+                                    <div class="form-inline">
 
-                                        {!! Form::password('password',  array('class'=>'span10 typeahead','id'=>'password'))!!}
+                                        {!! Form::password('password',  array('class'=>'form-control','id'=>'password'))!!}
 
                                     </div>
 
@@ -430,9 +423,9 @@
 
                                     {!!Form::label('password_confirmation', 'Confirm New password: *', array('class'=>'control-label', 'for'=>'typeahead'))!!}
 
-                                    <div class="controls">
+                                    <div class="form-inline">
 
-                                        {!! Form::password('password_confirmation',  array('class'=>'span10 typeahead','id'=>'password_confirmation'))!!}
+                                        {!! Form::password('password_confirmation',  array('class'=>'form-control','id'=>'password_confirmation'))!!}
 
                                     </div>
 
@@ -456,17 +449,17 @@
                             @endif
                       @if(Auth::user()->type_id==3)
                         <div class="clearfix"></div>
-                               <div class="controls control-group" style="margin-left:0px;">
+                               <div class="form-inline control-group" style="margin-left:0px;">
                                         Service Zipcodes(Comma seprated)
                                         <div class="control-group">
-                                            {!! Form::textarea('available_zipcodes', isset($user_data['available_zipcodes']) ? $user_data['available_zipcodes'] : '' , array('class'=>'span10 typeahead','id'=>'available_zipcodes'))!!}
+                                            {!! Form::textarea('available_zipcodes', isset($user_data['available_zipcodes']) ? $user_data['available_zipcodes'] : '' , array('class'=>'form-control','id'=>'available_zipcodes'))!!}
 
                                         </div>
                               </div>
                               @endif
 
 
-                            <div class="form-actions text-right clearfix">
+                            <div class="form-actions text-left" style="margin-top:20px !important;">
 
                                 {!! Form::submit('Save', array('class'=>'btn btn-large btn-success text-left','id'=>'profileSaveButton'))!!}
 
