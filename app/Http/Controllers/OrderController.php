b1aeffb9ee0db0452c3658f520b9a0ee03e4d77b
Jamie Dunn <jdunn82k@gmail.com>
2018-01-12 09:21:23 -0500

    Bug testing

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -63,8 +63,14 @@ class OrderController extends Controller
             $allservices = Service::getAllServices();
         }
 
+        //Get City/State Information
+        $city = City::where('id', $property_details[0]->city_id)->get()[0]->name;
+        $state = State::where('id', $property_details[0]->state_id)->get()[0]->name;
+
         return view('common.edit_order')
             ->with('order_details', $order_details)
+            ->with('city', $city)
+            ->with('state', $state)
             ->with('property_details', $property_details[0])
             ->with('vendorsDATA', $vendorsDATA)
             ->with('order', $data)
